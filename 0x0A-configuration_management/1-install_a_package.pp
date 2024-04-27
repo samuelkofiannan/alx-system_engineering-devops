93c28440740cd1859b86f6365fdc1a609f2cdda4
Kofi <qouphieligal@gmail.com>
2024-04-27 05:51:53 -0700

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,19 +1,6 @@
-# Install Python3
-package { 'python3.8':
-  ensure   => '3.8.10',
-  provider   => 'apt',
-}
-
 # Install Flask 2.1.0 using pip3
 package { 'flask':
   ensure   => '2.1.0',
-  provider   => 'pip',
-  require   => Package['python3.8-pip'],
-}
-
-# Install Werkzeug 2.1.1 using pip3
-package { 'werkzeug':
-  ensure   => '2.1.1',
   provider   => 'pip',
   require   => Package['python3.8-pip'],
 }
