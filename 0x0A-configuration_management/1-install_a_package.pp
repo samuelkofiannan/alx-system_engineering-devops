4a85a23ec977a0c351d0e4b86240402865901ec1
Kofi <qouphieligal@gmail.com>
2024-04-27 05:31:34 -0700

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,25 +1,6 @@
-# Install Python3
-package { 'python3.8':
-  ensure   => '3.8.10',
-  provider   => 'apt',
-}
-
-# Install pip3 for Python3.8
-package { 'python3.8-pip':
-  ensure   => '21.2.4', # ensure pip3 version 21.2.4 is installed
-  provider   => 'apt',
-}
-
-# Install Flask 2.1.0 using pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider   => 'pip',
-  require   => Package['python3.8-pip'],
-}
-
-# Install Werkzeug 2.1.1 using pip3
-package { 'werkzeug':
-  ensure   => '2.1.1',
-  provider   => 'pip',
-  require   => Package['python3.8-pip'],
+# this Puppet manifest installs the puppet-lint package
+package {'puppet-lint':
+  ensure   => 'present',
+  version  => '2.1.1',
+  provider => 'puppet_gem',
 }
