db86099c12b7aa31caeee392da5369c4dab19c83
Kofi <qouphieligal@gmail.com>
2024-04-27 05:40:31 -0700

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,13 @@
-# this Puppet manifest installs the puppet-lint package
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Install an especific version of flask (2.1.0)
+package { 'Python3': # package to be installed
+  ensure   => '3.8.10', # installation of the latest version of package
+  provider   => 'pip3', # provider to install package
+}
+package { 'flask': # package to be installed
+  ensure   => '2.1.0', # installation of the lastest version of package
+  provider   => 'pip3', # provider to install package
+}
+package { 'Werkzeug': # package to be installed
+  ensure   => '2.1.1', # installation of the latest version of package
+  provider   => 'pip3', # provider to install package
 }
