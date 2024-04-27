a858a534131398a57a2c0faa2f1db4ef3cc3a36f
Kofi <qouphieligal@gmail.com>
2024-04-27 05:54:50 -0700

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Install Flask 2.1.0 using pip3
+# Install flask v2.1.0 from pip3
 package { 'flask':
   ensure   => '2.1.0',
-  provider   => 'pip',
-  require   => Package['python3.8-pip'],
+  provider => 'pip3',
 }
